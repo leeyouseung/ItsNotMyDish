1ebd5078a0e545764dc3cb08f5fb29c49c6c01d5
Hyo-Seong <dlgytjd0907@naver.com>
2018-11-10 23:21:06 +0900

    add MainMapActivity

---
+++ app/src/main/java/com/example/admin/itsnotmydish/MainMapActivity.java
@@ -0,0 +1,13 @@
+package com.example.admin.itsnotmydish;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainMapActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_map);
+    }
+}
