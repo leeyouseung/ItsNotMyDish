c12631d837eda7b3489d8b8a18d29eeca341d92e
Hyo-Seong <dlgytjd0907@naver.com>
2018-11-10 23:31:49 +0900

    add map lib

--- app/src/main/java/com/example/admin/itsnotmydish/MainMapActivity.java
+++ app/src/main/java/com/example/admin/itsnotmydish/MainMapActivity.java
@@ -2,6 +2,9 @@ package com.example.admin.itsnotmydish;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.ViewGroup;
+
+import net.daum.mf.map.api.MapView;
 
 public class MainMapActivity extends AppCompatActivity {
 
@@ -9,5 +12,10 @@ public class MainMapActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_map);
+
+        MapView mapView = new MapView(this);
+
+        ViewGroup mapViewContainer = (ViewGroup) findViewById(R.id.map_view);
+        mapViewContainer.addView(mapView);
     }
 }
